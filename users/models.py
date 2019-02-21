9b287011a8a18c641c4d102b7739b5546e465bd3
Indulgent丶徴 <1343837706@qq.com>
2019-02-21 22:23:40 +0800

    Usermodel添加邮箱验证码，首页轮播图。对应4-5

--- users/models.py
+++ users/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -24,10 +26,10 @@ class UserProfile(AbstractUser):
     address = models.CharField(max_length=100, verbose_name="地址", default="")
     # 电话
     mobile = models.CharField(max_length=11, null=True, blank=True)
-    # 头像 默认使用default.png
+    # 头像 默认使用default.jpg
     image = models.ImageField(
         upload_to="image/%Y/%m",
-        default=u"image/default.png",
+        default=u"image/default.jpg",
         max_length=100
     )
 
@@ -39,3 +41,54 @@ class UserProfile(AbstractUser):
     # 打印实例会打印username，username为继承自abstractuser
     def __str__(self):
         return self.username
+
+
+class EmailVerifyRecord(models.Model):
+    """邮箱验证码model"""
+    SEND_CHOICES = (
+        ("register", u"注册"),
+        ("forget", u"找回密码"),
+        ("update_email", u"修改邮箱"),
+    )
+    code = models.CharField(max_length=20, verbose_name=u"验证码")
+    # 未设置null = true blank = true 默认不可为空
+    email = models.EmailField(max_length=50, verbose_name=u"邮箱")
+    send_type = models.CharField(
+        choices=SEND_CHOICES,
+        max_length=20,
+        verbose_name=u"验证码类型")
+    # 这里的now得去掉(),不去掉会根据编译时间。而不是根据实例化时间。
+    send_time = models.DateTimeField(
+        default=datetime.now, verbose_name=u"发送时间")
+
+    class Meta:
+        verbose_name = "邮箱验证码"
+        verbose_name_plural = verbose_name
+
+    # 重载str方法使后台不再直接显示object
+
+    def __str__(self):
+        return '{0}({1})'.format(self.code, self.email)
+
+
+class Banner(models.Model):
+    """轮播图model"""
+    title = models.CharField(max_length=100, verbose_name=u"标题")
+    image = models.ImageField(
+        upload_to="banner/%Y/%m",
+        verbose_name=u"轮播图",
+        max_length=100)
+    url = models.URLField(max_length=200, verbose_name=u"访问地址")
+    # 默认index很大靠后。想要靠前修改index值。
+    index = models.IntegerField(default=100, verbose_name=u"顺序")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
+
+    class Meta:
+        verbose_name = u"轮播图"
+        verbose_name_plural = verbose_name
+
+    # 重载__str__方法使后台不再直接显示object
+
+    def __str__(self):
+        return '{0}(位于第{1}位)'.format(self.title, self.index)
+
