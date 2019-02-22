e7eb444dde4fc0e96685d900cc51d8061b3a0696
Indulgent丶徴 <1343837706@qq.com>
2019-02-22 18:20:22 +0800

    Xadmin的安装与源码安装，配置setting中extra_apps. 对应5-2

--- mxEdu_online/settings.py
+++ mxEdu_online/settings.py
@@ -16,7 +16,7 @@ import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
-
+sys.path.insert(0, os.path.join(BASE_DIR, 'extra_apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     'users',
     'courses',
     'organization',
+    'xadmin',
+    'crispy_forms',
+    'DjangoUeditor',
+
 ]
 
 # 此处重载是为了使我们的UserProfile生效
